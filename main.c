a982da8804aa9f359ce22dda15f3050d386fcc9b
Vincent de Vos <vin.de.vos@gmail.com>
2013-04-29 13:54:31 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,48 @@
+#include <sys/time.h>
+#include <assert.h>
+
+#include "lifo.h"
+
+unsigned long long get_timestamp()
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return ((unsigned long long)(tv.tv_sec) * 1000) + ((unsigned long long)(tv.tv_usec));
+}
+
+
+void start(void)
+{
+    push(get_timestamp());
+}
+
+unsigned long long stop(void)
+{
+    unsigned long long ts_end = get_timestamp();
+    unsigned long long ts_start = popts();
+    return ts_end - ts_start;
+}
+
+void function_one() {
+
+    printf("Start of function_one\n");
+    start();
+
+    /* Do some stuff */
+
+    unsigned long long duration = stop();
+    printf("End of function_one - duration: %llu\n", duration);
+}
+
+int main(int argc, char** argv)
+{
+    lifo_init();
+
+    int i;
+    for(i = 0; i < 10; i++) {
+        function_one();
+    }
+
+    lifo_free();
+    return 0;
+}
