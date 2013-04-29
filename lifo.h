d4ebdcbc600146107e87194fa2f2cc2ab5ac552d
Vincent de Vos <vin.de.vos@gmail.com>
2013-04-29 13:55:04 +0300

    Create lifo.h

---
+++ lifo.h
@@ -0,0 +1,36 @@
+/*   
+* A LIFO Linked-list implementation 
+* The list holds microsecond timestamps of type: unsigned long long
+* Note: popts() free's the top list-item and returns it's timestamp 
+*/
+
+#ifndef LC_LIFO_H
+#define LC_LIFO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct item
+{
+  unsigned long long timestamp;
+  struct item* next;
+};
+struct buffer { struct item* head; };
+
+void lifo_init(void);
+void lifo_free(void);
+
+struct buffer* push( unsigned long long ts );
+struct buffer* pop( );
+unsigned long long popts( );
+struct item* top( struct buffer* );
+struct item* make_null( struct buffer* );
+struct item* is_empty( struct buffer* );
+
+void buffer_print( );
+void buffer_print_element( const struct item* );
+struct buffer* buffer_new( void );
+struct buffer* buffer_free( struct buffer* );
+
+#endif
